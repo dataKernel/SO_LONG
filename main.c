d2497ac0ca43a3a439356abe0049c15846a034cf
dataKernel <lancelot.saintleger@gmail.com>
2023-02-28 16:21:22 +0000

    fix: checking sprite info

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int     main(void)
 
 	window = create_window(1400, 700, "solong.exe");
 	sprite = create_sprite("assets/sprites/ground_1.xpm", window.mlxPtr);
+	printf("width --> %i", sprite.width);
+	printf("height --> %i", sprite.height);
+
 
 	return(0);
 }
