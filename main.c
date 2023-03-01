889ce437c81c4042f85be418e9c3d2c0940ca698
dataKernel <lancelot.saintleger@gmail.com>
2023-03-01 09:23:27 +0000

    fix: updating with refacto on main struct

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "include/image.h"
+#include "include/image_utils.h"
 
 int     main(void)
 {
 	t_win 	window;
+	t_img 	map;
 	t_img 	sprite;
 
 	window = create_window(1400, 700, "solong.exe");
-	sprite = create_sprite("assets/sprites/ground_1.xpm", window.mlxPtr);
-	printf("width --> %i", sprite.width);
-	printf("height --> %i", sprite.height);
-
+	map = create_image(window.width, window.height, window.mlxPtr);
+	sprite = create_sprite("assets/sprites/character.xpm", window.mlxPtr);
+	//we copy the sprite inside the map
+	copy_sprite_in_image(sprite, map, 0, 0);
+	//pushing the image map in the window
+	mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
+	mlx_loop(window.mlxPtr);
 
 	return(0);
 }
