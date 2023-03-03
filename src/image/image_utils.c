c70325f78186df67a49cad8c6c749bc64b6db985
dataKernel <lancelot.saintleger@gmail.com>
2023-03-03 19:05:09 +0000

    fix: fixing pixel alignement on function copy_sprite_in_image()

--- src/image/image_utils.c
+++ src/image/image_utils.c
@@ -4,7 +4,7 @@
 
 unsigned int 	ft_pixel_get(t_img image, int x, int y)
 {
-	char 	*dst = image.addr + (y *image.lineLength + x * (image.bpp / 8));
+	char 	*dst = image.addr + (y *image.lineLength + (x * (image.bpp / 8)));
 	return *(unsigned int *)dst;
 }
 
@@ -12,7 +12,7 @@ void            ft_pixel_put(t_img image, int x, int y, int color)
 {
 	char 	*dst;
 
-	dst = image.addr + (y * image.lineLength + x * (image.bpp / 8));
+	dst = image.addr + (y * image.lineLength + (x * (image.bpp / 8)));
 	*(unsigned int*)dst = color;
 }
 
@@ -21,31 +21,21 @@ void 			copy_sprite_in_image(t_img sprite, t_img image, int x, int y)
 	int 	line;
 	int 	pixel;
 
-	line = y;
-	while(line < sprite.height)
+
+	line = 0;
+	while(line <= sprite.height)
 	{
-		printf("line --> %i\n", line);
-		printf("pixel --> %i\n", pixel);
-		pixel = x;
-		while(pixel < sprite.width)
+		pixel = 0;
+		while(pixel <= sprite.width)
 		{
-			ft_pixel_put(image, pixel, line, ft_pixel_get(sprite, pixel, line));
+			ft_pixel_put(image, x + pixel, y + line, ft_pixel_get(sprite, pixel, line));
 			pixel++;
 		}
 		line++;
 	}
 }
 
-
-/*
-while(ligne < sprite.height)
-    {
-        pixel = 0;
-        while(pixel < sprite.width)
-        {
-            ft_pixel_put(&map, 15 + pixel, 15 + ligne, ft_pixel_get(sprite, pixel, ligne));
-            pixel++;
-        }
-        ligne++;
-    }
-*/
+void 	transform_sprite_trans(t_img sprite)
+{
+	//...
+}
