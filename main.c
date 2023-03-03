e0ce0f15c630720bd24355e629b18df5535dbda3
dataKernel <lancelot.saintleger@gmail.com>
2023-03-03 19:04:27 +0000

    fix: updating legacy code with filling full map generation + fixing pixel alignement issues

--- main.c
+++ main.c
@@ -2,17 +2,37 @@
 #include "include/image.h"
 #include "include/image_utils.h"
 
+
+
+
 int     main(void)
 {
 	t_win 	window;
 	t_img 	map;
-	t_img 	sprite;
+	t_img 	spriteGround1;
+	t_img 	spriteChar;
 
-	window = create_window(1400, 700, "solong.exe");
+	int 	line;
+	int 	height;
+
+	height = 0;
+	window = create_window(800, 800, "solong.exe");
 	map = create_image(window.width, window.height, window.mlxPtr);
-	sprite = create_sprite("assets/sprites/character.xpm", window.mlxPtr);
-	//we copy the sprite inside the map
-	copy_sprite_in_image(sprite, map, 0, 0);
+	spriteGround1 = create_sprite("assets/sprites/ground_1.xpm", window.mlxPtr);
+	spriteChar = create_sprite("assets/sprites/character.xpm", window.mlxPtr);
+	//we copy the spriteGround inside the map
+	while(height < map.height)
+	{
+		line = 0;
+		while(line < map.width)
+		{
+			copy_sprite_in_image(spriteGround1, map, 0 + line, 0 + height);
+			line += spriteGround1.width;
+		}
+		height += spriteGround1.height;
+	}
+	//we copy the character inside the map
+	copy_sprite_in_image(spriteChar, map, 360, 160);
 	//pushing the image map in the window
 	mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
 	mlx_loop(window.mlxPtr);
