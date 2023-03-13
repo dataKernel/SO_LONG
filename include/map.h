271e0cb11f4f10379ca3453f9f685687570e4633
dataKernel <lancelot.saintleger@gmail.com>
2023-03-13 10:48:49 +0000

    feat: new files map (src/head)

---
+++ include/map.h
@@ -0,0 +1,6 @@
+#ifndef MAP_H
+# define MAP_H
+
+generate_map(char *map, void *mlxPtr);
+
+#endif
---
+++ src/map/map.c
@@ -0,0 +1,6 @@
+#include "../../include/map.h"
+
+void    generate_map(char *map, void *mlxPtr)
+{
+    load_sprite(mlxPtr);
+}
