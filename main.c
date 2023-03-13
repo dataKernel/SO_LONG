5a740d914d3343f803b81817cc427da26b7f9471
dataKernel <lancelot.saintleger@gmail.com>
2023-03-13 17:29:54 +0000

    fix: reorganizing the main with structuration

--- main.c
+++ main.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
+
 #include "include/map_checking_utils.h"
 #include "include/image.h"
 #include "include/image_utils.h"
-
+#include "include/map.h"
 
 int     main(void)
 {
 	t_win 	window;
+	t_img 	map;
+	char 	*mapString;
+	int 	winWidth;
+	int 	winHeight;
 
-	window = create_window(405, 490, "solong.exe");
 
-	//checking the ptr related to win
-	printf("ptr mlx --> %p\n", window.mlxPtr);
-	//checking the width and height
-	printf("w:%i_____h:%i\n", window.width, window.height);
-
-	//generate_map("assets/maps/map.ber");
-	//we copy the spriteGround inside the map
-	/*while(height < map.height)
-	{
-		line = 0;
-		while(line < map.width)
-		{
-			copy_sprite_in_image(spriteGround1, map, 0 + line, 0 + height);
-			line += spriteGround1.width;
-		}
-		height += spriteGround1.height;
-	}*/
-	//pushing the image map in the window
-	//mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
-	//mlx_loop(window.mlxPtr);
+	mapString = read_map("assets/maps/map.ber");
+	winWidth = check_width_line(mapString);
+	winHeight = check_height_column(mapString);
+	window = create_window(winWidth, winHeight, "so_long.exe");
+	generate_map(mapString, window.mlxPtr);
 	return(0);
 }
+
+
+
+
+
+
+
+
+//generate_map("assets/maps/map.ber");
+//we copy the spriteGround inside the map
+/*while(height < map.height)
+{
+	line = 0;
+	while(line < map.width)
+	{
+		copy_sprite_in_image(spriteGround1, map, 0 + line, 0 + height);
+		line += spriteGround1.width;
+	}
+	height += spriteGround1.height;
+}*/
+//pushing the image map in the window
+//mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
+//mlx_loop(window.mlxPtr);
