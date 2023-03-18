2349dd5a48c2e4ea502cc1a2e1e3740147d1ef5a
dataKernel <lancelot.saintleger@gmail.com>
2023-03-18 14:10:30 +0000

    fix: updating the main (function call and checking values + new resolutions with the 40*40px)

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int     main(void)
 
 	mapString = read_map("assets/maps/map.ber");
 	winWidth = check_width_line(mapString) * 40;
-	winHeight = (check_height_column(mapString) - 1) * 40 + 163;
+	winHeight = (check_height_column(mapString) - 1) * 40 + 82;
 	window = create_window(winWidth, winHeight, "so_long.exe");
 	map = generate_map(mapString, window);
 	mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
@@ -23,26 +23,3 @@ int     main(void)
 
 	return(0);
 }
-
-
-
-
-
-
-
-
-//generate_map("assets/maps/map.ber");
-//we copy the spriteGround inside the map
-/*while(height < map.height)
-{
-	line = 0;
-	while(line < map.width)
-	{
-		copy_sprite_in_image(spriteGround1, map, 0 + line, 0 + height);
-		line += spriteGround1.width;
-	}
-	height += spriteGround1.height;
-}*/
-//pushing the image map in the window
-//mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
-//mlx_loop(window.mlxPtr);
