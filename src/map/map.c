3fe3fb62c93eb5e3b9c62cc2d903c91e2691b397
dataKernel <lancelot.saintleger@gmail.com>
2023-03-19 18:31:53 +0000

    fix: updating the map generation to load all sprites :: need to fix the map size extra column unnacuracy

--- src/map/map.c
+++ src/map/map.c
@@ -4,8 +4,11 @@
 #include "../../include/map.h"
 #include "../../include/map_checking_utils.h"
 
-void    generate_wall_top(t_img map, t_img *spriteTabPtr, int line)
+void    generate_wall_top(t_img map, t_img *spriteTabPtr)
 {
+    int     line;
+
+    line = 0;
     while(line < map.width)
     {
         copy_sprite_in_image(spriteTabPtr[3], map, 0 + line, 0);
@@ -13,16 +16,28 @@ void    generate_wall_top(t_img map, t_img *spriteTabPtr, int line)
     }
 }
 
-void    generate_map_content(t_img map, t_img *spriteTabPtr, int line, int height)
+void    generate_map_content(char *mapString, t_img map, t_img *spriteTabPtr)
 {
+    int     height;
+    int     line;
+    int     i;
+
+    i = check_width_line(mapString);
+    height = spriteTabPtr[3].height;
     while(height < map.height)
     {
     	line = 0;
     	while(line < map.width)
     	{
-    		copy_sprite_in_image(spriteTabPtr[1], map, 0 + line, 0 + height);
-    		line += spriteTabPtr[1].width;
-    	}
+            if(mapString[i] == SPACE)
+    		    copy_sprite_in_image(spriteTabPtr[GROUND_1], map, 0 + line, 0 + height);
+            else if(mapString[i] == WALL)
+                copy_sprite_in_image(spriteTabPtr[WALL_TREE_2], map, 0 + line, 0 + height);
+            else if(mapString[i] == COLLECTIBLE)
+                copy_sprite_in_image(spriteTabPtr[RESSOURCE_1], map, 0 + line, 0 + height);
+            line += spriteTabPtr[1].width;
+            i++;
+        }
     	height += spriteTabPtr[1].height;
     }
 }
@@ -31,17 +46,11 @@ t_img    generate_map(char *mapString, t_win window)
 {
     t_img   *spriteTabPtr;
     t_img   map;
-    int     widthMap;
-    int     heightMap;
-    int     line;
-    int     height;
 
-    line = 0;
-    height = 82;
     map = create_image(window.width, window.height, window.mlxPtr);
     spriteTabPtr = load_sprite(window.mlxPtr);
-    generate_wall_top(map, spriteTabPtr, line);
-
+    generate_wall_top(map, spriteTabPtr);
+    generate_map_content(mapString, map, spriteTabPtr);
     free(spriteTabPtr);
 
     return(map);
