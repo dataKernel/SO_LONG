2a3c3e0d15d609b51c4dea6320aef7f623ec3a79
dataKernel <lancelot.saintleger@gmail.com>
2023-03-20 19:46:48 +0000

    fix: updating the main with x/y char position and overall code to implement the base lineé

--- main.c
+++ main.c
@@ -7,12 +7,15 @@
 
 int     main(void)
 {
-	t_win 	window;
 	t_img 	character;
+	t_win 	window;
 	t_img 	map;
 	char 	*mapString;
 	int 	winHeight;
 	int 	winWidth;
+	//posi from the map char inclusion
+	int 	x;
+	int 	y;
 
 	mapString = read_map("assets/maps/map.ber");
 	winWidth = check_width_line(mapString) * 40;
@@ -20,9 +23,13 @@ int     main(void)
 	window = create_window(winWidth, winHeight, "so_long.exe");
 	map = generate_map(mapString, window);
 	character = create_sprite(CHAR_1_PATH, window.mlxPtr);
+	x = get_position_character(mapString) / check_width_line(mapString) - 1;
+	y = get_position_character(mapString) % check_height_column(mapString);
+	// printf("char posi : %i\n", get_position_character(mapString));
+	// printf("largeur: %i, hauteur: %i\n", check_width_line(mapString), check_height_column(mapString));
+	// printf("x:%i, y:%i", x, y);
 	mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
-	//on push le personnage sur la posi de map
-	mlx_put_image_to_window(window.mlxPtr, window.winPtr, character.imgPtr, )
+	mlx_put_image_to_window(window.mlxPtr, window.winPtr, character.imgPtr, x * 40, y * 40);
 	mlx_loop(window.mlxPtr);
 	return(0);
 }
