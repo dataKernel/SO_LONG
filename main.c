e58335be0ff50b1e8e54de597b611425753b168e
dataKernel <lancelot.saintleger@gmail.com>
2023-03-21 14:51:32 +0000

    fix: updating the main with correct posi on char and adding commentary to refactoring later on

--- main.c
+++ main.c
@@ -17,19 +17,19 @@ int     main(void)
 	int 	x;
 	int 	y;
 
+	//map generation et windows size
 	mapString = read_map("assets/maps/map.ber");
 	winWidth = check_width_line(mapString) * 40;
 	winHeight = (check_height_column(mapString) - 1) * 40 + 82;
 	window = create_window(winWidth, winHeight, "so_long.exe");
 	map = generate_map(mapString, window);
+	//ajout du personnage
 	character = create_sprite(CHAR_1_PATH, window.mlxPtr);
-	x = get_position_character(mapString) / check_width_line(mapString) - 1;
-	y = get_position_character(mapString) % check_height_column(mapString);
-	// printf("char posi : %i\n", get_position_character(mapString));
-	// printf("largeur: %i, hauteur: %i\n", check_width_line(mapString), check_height_column(mapString));
-	// printf("x:%i, y:%i", x, y);
+	x = get_position_character(mapString) % check_width_line(mapString) * 40;
+	y = get_position_character(mapString) / check_width_line(mapString) * 40 + 42;
+	//push des elems dans la fenetre
 	mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
-	mlx_put_image_to_window(window.mlxPtr, window.winPtr, character.imgPtr, x * 40, y * 40);
+	mlx_put_image_to_window(window.mlxPtr, window.winPtr, character.imgPtr, x, y);
 	mlx_loop(window.mlxPtr);
 	return(0);
 }
