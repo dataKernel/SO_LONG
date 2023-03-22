5fc6d64d38cfefe87b5468cbb7b444d5a3eaee28
dataKernel <lancelot.saintleger@gmail.com>
2023-03-22 16:35:02 +0000

    fix: updating the map

--- main.c
+++ main.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 #include "include/map_checking_utils.h"
-#include "include/image.h"
 #include "include/image_utils.h"
+#include "include/character.h"
+#include "include/image.h"
 #include "include/map.h"
 
 int     main(void)
 {
-	t_img 	character;
+	t_char 	character;
 	t_win 	window;
 	t_img 	map;
 	char 	*mapString;
 	int 	winHeight;
 	int 	winWidth;
-	//posi from the map char inclusion
-	int 	x;
-	int 	y;
 
 	//init seed
 	srand(time(NULL));
@@ -25,13 +23,11 @@ int     main(void)
 	winHeight = (check_height_column(mapString) - 1) * 40 + 82;
 	window = create_window(winWidth, winHeight, "so_long.exe");
 	map = generate_map(mapString, window);
-	//ajout du personnage
-	character = create_sprite(CHAR_1_PATH, window.mlxPtr);
-	x = get_position_character(mapString) % check_width_line(mapString) * 40;
-	y = get_position_character(mapString) / check_width_line(mapString) * 40 + 42;
+	character = load_character(window, mapsString);
+	//push du perso
 	//push des elems dans la fenetre
 	mlx_put_image_to_window(window.mlxPtr, window.winPtr, map.imgPtr, 0, 0);
-	mlx_put_image_to_window(window.mlxPtr, window.winPtr, character.imgPtr, x, y);
+
 	mlx_loop(window.mlxPtr);
 	return(0);
 }
