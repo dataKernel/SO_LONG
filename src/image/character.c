8827d91206c55dc3366b95e8817903b8ee349ca9
dataKernel <lancelot.saintleger@gmail.com>
2023-03-22 17:31:15 +0000

    fix: updating the typdef redefinition

---
+++ src/image/character.c
@@ -0,0 +1,29 @@
+#include "../../include/image.h"
+#include "../../include/character.h"
+#include "../../include/map_checking_utils.h"
+
+t_char     load_character(t_win window, char *mapString)
+{
+    t_char     character;
+
+    character.sprite = create_sprite(CHAR_1_PATH, window.mlxPtr);
+    character.posiX = get_position_character(mapString) % check_width_line(mapString);
+    character.posiY = get_position_character(mapString) / check_width_line(mapString);
+    return(character);
+}
+
+int             get_position_character(char *mapString)
+{
+    int     countBackSlash;
+    int     i;
+
+    i = 0;
+    countBackSlash = 0;
+    while(mapString[i] != PLAYER)
+    {
+        if(mapString[i] == '\n')
+        countBackSlash++;
+        i++;
+    }
+    return(i - countBackSlash);
+}
