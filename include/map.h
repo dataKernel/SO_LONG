20f48eb03040508a26ee86a03a7a37510c2ab7a4
dataKernel <lancelot.saintleger@gmail.com>
2023-03-28 09:30:14 +0100

    fix: updating the header map and map_checking_utils.h

--- include/map.h
+++ include/map.h
@@ -5,6 +5,13 @@
 # include <time.h>
 # include "image.h"
 
+typedef struct      s_map
+{
+    int     widht;
+    int     height;
+    char    *content;
+}t_map;
+
 t_img       load_map(char *pathMap, t_win window);
 void        generate_wall_top(t_img map, t_img *spriteTabPtr);
 void        generate_map_content(char *mapString, t_img map, t_img *spriteTabPtr);
