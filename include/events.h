2f17d2195a80aeee1db718f02b7da06af6203cdf
t_void *data_kernel[42] <lancelot.saintleger@gmail.com>
2023-04-12 12:42:49 +0100

    fix : updating map.ber with small size to debug matrix map + updating protos in events.h

--- include/events.h
+++ include/events.h
@@ -18,7 +18,8 @@ typedef struct      s_events
     t_map   map;
 }t_events;
 
-void    move_char(t_char *character, char *map, int widthLine, int keycode, void *mlx);
+void    move_char_position_top(char *map, t_char *character, int position);
+void    move_char(int direction, t_events *events);
 int     hook_character(int keycode, t_events *events);
 int     check_key(int keycode);
 
